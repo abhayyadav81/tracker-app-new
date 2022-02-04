82f7d33738d36717e8a4bfe66c72d2e1714532c6
abhayyadav81 <withabhay81@gmail.com>
2022-02-04 11:54:13 +0530

    poert changed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ app.get('/',(req,res)=>{
     res.send('hii abhay groot')
 
 })
-app.listen(3000,()=>{
-    console.log('lestening on port 3000')
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=>{
+    console.log('lestening on port '+ PORT)
 })
