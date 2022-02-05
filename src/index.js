f8fb39a9cb3694b0ca35cee37fe402f9a41842e4
abhayyadav81 <withabhay81@gmail.com>
2022-02-05 11:23:34 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,41 @@
+require("./models/User");
+require("./models/Track");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri = "mongodb://abhayyadav:68700687@cluster0-shard-00-00.xewp9.mongodb.net:27017,cluster0-shard-00-01.xewp9.mongodb.net:27017,cluster0-shard-00-02.xewp9.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-481tvg-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+// if (!mongoUri) {
+//   throw new Error(
+//     `MongoURI was not supplied.  Make sure you watch the video on setting up Mongo DB!`
+//   );
+// }
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3001, () => {
+  console.log("Listening on port 3001");
+});
